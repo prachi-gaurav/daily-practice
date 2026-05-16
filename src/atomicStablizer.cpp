358af5913997adde5fee6996cbea81d6f3e0b267
Prachi🍁 <prachigaurav10@gmail.com>
2026-05-17 00:07:34 +0530

    Add problem statement for atmospheric stabilizer
    Added problem statement for atmospheric stabilizer.

---
+++ src/atomicStablizer.cpp
@@ -0,0 +1,11 @@
+/* DATE: 16th May 2026
+Problem Statement: Problem Statement: "The Atmospheric Stabilizer"You are monitoring a planetary atmospheric stabilizer. You receive an array of integers P, representing hourly pressure 
+fluctuations. These fluctuations can be positive (pressure increase), negative (pressure decrease), or zero (no change). The stabilizer relies on a specific sequence to power up: it requires 
+a contiguous block of hours where the net sum of the pressure fluctuations is exactly equal to a critical target threshold, T. Your commander needs to know the total number of distinct 
+contiguous blocks of hours that sum to exactly T.
+Constraints:
+1<=N<=10^5 (The number of hours)
+-1000<=P[i]<=1000 (The pressure change at hour $i$)
+-10^7<=T<=10^7 (The target threshold)
+Time Limit: 1.0s
+*/
